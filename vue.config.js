e5ba35e1784ac84a9daac84dec5acb8d0f73a30d
Felipe Marinho <felipevm97@gmail.com>
2020-03-24 20:45:54 -0300

    skip bundle analyzer by default

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const plugins = [];
+
+if (process.env.ANALYZE == "true") {
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = {
   publicPath: "/",
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
+    plugins
   }
 };
